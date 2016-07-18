d7846d88d693c9370c7266bc444cffc41dfc70ba
SuperDade <342151955@qq.com>
2016-07-18 16:04:18 +0800

    添加一个dog

---
+++ src/main/java/cn/net/wangchenyu/finance/dao/ClosingCostDao.java
@@ -0,0 +1,9 @@
+package lists;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by lenovo on 2016/7/17.
+ */
+public interface ClosingCostDao extends CrudRepository<ClosingCost,Integer> {
+}
