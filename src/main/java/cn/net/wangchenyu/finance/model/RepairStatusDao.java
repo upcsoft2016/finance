d83c597d39fbd382f4942ad61a72229c026c7390
cheneyveron <cheneyveron@live.cn>
2016-07-18 16:20:54 +0800

    删除ReportStatusDao

--- src/main/java/cn/net/wangchenyu/finance/model/RepairStatusDao.java
+++
@@ -1,9 +0,0 @@
-package lists;
-
-import org.springframework.data.repository.CrudRepository;
-
-/**
- * Created by lenovo on 2016/7/18.
- */
-public interface RepairStatusDao  extends CrudRepository <RepairStatus,Integer> {
-}
