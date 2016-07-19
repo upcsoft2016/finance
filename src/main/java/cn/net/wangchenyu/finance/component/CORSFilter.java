b2f7ab62cd20d9d996d76e043a5d6424a0569db3
cheneyveron <cheneyveron@live.cn>
2016-07-19 16:36:03 +0800

    取消域限制，提供CustomManage的交互例子。

---
+++ src/main/java/cn/net/wangchenyu/finance/component/CORSFilter.java
@@ -0,0 +1,7 @@
+package cn.net.wangchenyu.finance.component;
+
+/**
+ * Created by cheneyveron on 7/19/16.
+ */
+public class CORSFilter {
+}
---
+++ src/main/java/cn/net/wangchenyu/finance/model/ReturnMessage.java
@@ -0,0 +1,7 @@
+package cn.net.wangchenyu.finance.model;
+
+/**
+ * Created by cheneyveron on 7/19/16.
+ */
+public class ReturnMessage {
+}
