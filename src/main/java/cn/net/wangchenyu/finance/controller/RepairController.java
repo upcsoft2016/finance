363d7bcc614b4efafc4c28e3426f7d3cd749f400
dhlongsir <2405902561@qq.com>
2016-07-20 18:03:17 +0800

    判断客服权限

--- src/main/java/cn/net/wangchenyu/finance/controller/RepairController.java
+++ src/main/java/cn/net/wangchenyu/finance/controller/RepairController.java
@@ -43,11 +43,41 @@ class RepairController {
             returnMessage.message = "请先登录!";
             return returnMessage;
         };
-
         //验证通过后保存到数据库
-        repairListDao.save(repairList);
-        returnMessage.id = 0;
-        returnMessage.message = "保存成功！";
+        if(authService.isAuthenticated()){
+            if(authService.getCurrentUserRole()){
+                if(toString().equals("客服"))
+            repairListDao.save(repairList);
+            returnMessage.id = 0;
+            returnMessage.message = "提交成功！";
+            }
+        }
         return returnMessage;
     }
+    /*class ReportInfo {
+        private int Maintenancenumber;     //维修编号
+        private int CustomerID;            //客户编号
+        private int Producttype;          //产品类型
+        private String IMVorDelta;        //机器品牌
+        private String Machinemodel;      //机器型号
+        private String Serialnumber;      //系列号
+        private String Serialnumber;     //系列号
+        private String Lackparts;         //缺少零件
+        private String Machinefaultpheno;//机器故障现象
+        private int Faulttype;          //故障类型
+        private String Appearanceinsp;   //机器外观检查
+        private String Bootpassword;     //开机口令
+        private String Importinformation;//重要资料
+        private String HDD;              //硬件
+        private String Memory;           //内存
+        private String PCcard;           //外置PC卡
+        private String Acadapter;        //ＡＣ适配器
+        private String Battery;           //电池
+        private String Externaldrives;    //外接光驱
+        private String Floppydrive;       //外接软驱
+        private String Others;            //其它
+        private String Repairtime;        //报修时间
+        private float Estimateprice;     //预估价格
+        private int Repairstatus;         //报修状态
+        }*/
 }
